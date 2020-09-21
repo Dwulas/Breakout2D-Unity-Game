b0b79993da8f12b4cceb13814780bd1e01f5c1d0
maciek <dwulas34@gmail.com>
2020-09-21 23:49:34 +0200

    Almost Done!

---
+++ Assets/BrickManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BrickManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static BrickManager _instance;
+
+    public static BrickManager Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    #endregion
+    public Sprite[] Sprites;
+    private int maxRows = 17;
+    private int maxCols = 12;
+
+    public List<int[,]> LevelsData { get; set; }
+
+    void Start()
+    {
+        this.LevelsData = this.LoadLevelData();
+    }
+
+    private List<int[,]> LoadLevelData()
+    {
+        TextAsset text = Resources.Load("levels") as TextAsset;
+        string[] rows = text.text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+        List<int[,]> levelsData = new List<int[,]>();
+        int[,] currentLevel = new int[maxRows, maxCols];
+        int currentRow = 0;
+
+        for(int row = 0; row < rows.Length; row++)
+        {
+            string line = rows[row];
+
+            if(line.IndexOf("--") == -1)
+            {
+                string[] bricks = line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                for(int col = 0; col < bricks.Length; col++)
+                {
+                    currentLevel[currentRow, col] = int.Parse(bricks[col]);
+                }
+
+                currentRow++;
+            }
+            else
+            {
+                currentRow = 0;
+                levelsData.Add(currentLevel);
+                currentLevel = new int[maxRows, maxCols];
+            }
+        }
+        return levelsData;
+    }
+
+    
+}
---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Start()
+    {
+        Respawn();
+    }
+
+    public void Respawn()
+    {
+        transform.position = Vector3.zero;
+        GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle.normalized * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Color randomlySelectedColor = GetRandomColor();
+        GetComponent<Renderer>().material.color = randomlySelectedColor;
+    }
+
+    private Color GetRandomColor()
+    {
+        return new Color(
+            r: UnityEngine.Random.Range(0f, 1f),
+            g: UnityEngine.Random.Range(0f, 1f),
+            b: UnityEngine.Random.Range(0f, 1f));
+    }
+
+}
